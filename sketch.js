3a4b4600691a488a4b8f4a083913c143f7e5cdbc
BlackBen2605 <72912407+BlackBen2605@users.noreply.github.com>
2021-01-29 10:38:33 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var canvas
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance=0;
+var database;
+var form, player, game;
+var sounds;
+var audio1
+var s1, s2;
+
+function preload(){
+  var audio1 = new Audio("sound/beep.wav");
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+
+if(playerCount === 2){
+  game.update(1);
+}
+if(gameState === 1){
+  clear();
+  game.play();
+  form.hide();
+  audio1.play();
+}
+if(gameState === 2){
+  game.end();
+}
+}
